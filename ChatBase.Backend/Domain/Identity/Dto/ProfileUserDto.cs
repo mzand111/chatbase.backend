4209577151d4b62180d81cd075fc59dd71bef18b
Mahdi Zand <80402823+mzand111@users.noreply.github.com>
2025-01-18 19:42:20 +0300

    login completed

--- ChatBase.Backend/Domain/Identity/Dto/ProfileUserDto.cs
+++ ChatBase.Backend/Domain/Identity/Dto/ProfileUserDto.cs
@@ -1,13 +1,10 @@
 ﻿using ChatBase.Backend.Helper;
-using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ChatBase.Backend.Domain.Identity.Dto;
 
 public class ProfileUserDto
 {
-    public Guid UserId { get; set; }
-    [MaxLength(100)]
     public string FirstName { get; set; }
     [MaxLength(100)]
     public string LastName { get; set; }
